222dcefe48be113b082c9512dadd19bb7c6dcaa6
Alex <b.boy.cherry@gmail.com>
2019-03-28 17:14:36 +0300

    Initial DB setup

--- backend/src/main/kotlin/com/alex/kob/KobApplication.kt
+++ backend/src/main/kotlin/com/alex/kob/KobApplication.kt
@@ -1,10 +1,20 @@
 package com.alex.kob
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters
+import java.util.*
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = [KobApplication::class, Jsr310JpaConverters::class])
 class KobApplication
+
+@PostConstruct
+internal fun init() {
+	TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+}
 
 fun main(args: Array<String>) {
 	runApplication<KobApplication>(*args)
