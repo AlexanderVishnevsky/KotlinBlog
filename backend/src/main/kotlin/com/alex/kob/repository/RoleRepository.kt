8a43315e8bdc948e28367c97512d37811c30ca6e
Alex <b.boy.cherry@gmail.com>
2019-04-10 16:19:03 +0300

    fix repo and controller

--- backend/src/main/kotlin/com/alex/kob/repository/RoleRepository.kt
+++ backend/src/main/kotlin/com/alex/kob/repository/RoleRepository.kt
@@ -3,9 +3,9 @@ package com.alex.kob.repository
 import com.alex.kob.model.Role
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import java.util.Optional
+import java.util.*
 
 @Repository
 interface RoleRepository : JpaRepository<Role, Long> {
-    fun findByName(roleName: String): Optional<Role>
+    fun findByRoleName(roleName: String): Optional<Role>
 }
